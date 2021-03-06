8507ec6354fe5d4cc9137db44292a864a63086ca
Serhat ZENGİN <srhtzngn@hotmail.com>
2021-03-06 11:40:36 +0300

    06/03/2021

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -12,49 +12,120 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  var scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Drawer'),
+
+     return  MaterialApp(
+      theme: ThemeData(
+        // Define the default Brightness and Colors
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+      ),
+      home: Scaffold(
+        key: scaffoldKey,
+        drawer: new Drawer(
+          child: new ListView(),
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(child: Text('Canım Drawerim')),
-              SocialLoginButton(
-                butonText: "Çıkış Yap",
-                butonColor: Color(0xffffffff),
-                textColor: Color(0xff868686),
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, LoginScreen.idScreen, (route) => false);
-                },
-                //butonIcon: Image.asset("images/facebook.png", width: 10),
-                butongenislik: 350,
-                butonyukseklik: 44,
+        body: Stack(
+          children: <Widget>[
+            new Center(
+                child: new Column(
+              children: <Widget>[],
+            )),
+            Positioned(
+              left: 10,
+              top: 20,
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () => scaffoldKey.currentState.openDrawer(),
               ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, LoginScreen.idScreen, (route) => false);
-                },
-                child: ListTile(
-                  title: Text('Deneme'),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            InkWell(
-                child: Container(
-                    height: 50, width: double.maxFinite, color: Colors.black)),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
+    // return Scaffold(
+    //   key: _scaffoldKey,
+    //   // appBar: AppBar(
+    //   //   title: Text('Drawer'),
+    //   // ),
+    //   drawer: Drawer(
+    //     child: ListView(
+    //       children: <Widget>[
+    //         DrawerHeader(child: Text('Canım Drawerim')),
+    //         SocialLoginButton(
+    //           butonText: "Çıkış Yap",
+    //           butonColor: Color(0xffffffff),
+    //           textColor: Color(0xff868686),
+    //           onPressed: () {
+    //             FirebaseAuth.instance.signOut();
+    //             Navigator.pushNamedAndRemoveUntil(
+    //                 context, LoginScreen.idScreen, (route) => false);
+    //           },
+    //           //butonIcon: Image.asset("images/facebook.png", width: 10),
+    //           butongenislik: 350,
+    //           butonyukseklik: 44,
+    //         ),
+    //         SizedBox(height: 10),
+    //         GestureDetector(
+    //           onTap: () {
+    //             FirebaseAuth.instance.signOut();
+    //             Navigator.pushNamedAndRemoveUntil(
+    //                 context, LoginScreen.idScreen, (route) => false);
+    //           },
+    //           child: ListTile(
+    //             title: Text('Deneme'),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+}
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // Define the default Brightness and Colors
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+      ),
+      home: Scaffold(
+        key: scaffoldKey,
+        drawer: new Drawer(
+          child: new ListView(),
+        ),
+        body: Stack(
+          children: <Widget>[
+            new Center(
+                child: new Column(
+              children: <Widget>[],
+            )),
+            Positioned(
+              left: 10,
+              top: 20,
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () => scaffoldKey.currentState.openDrawer(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
